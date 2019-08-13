8dd97c1ed5b92370852b9c703d96ca938dbab95e
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-08-13 18:13:56 +0100

    add connectionString

--- server/__db/db-config.js
+++ server/__db/db-config.js
@@ -7,11 +7,6 @@ module.exports = {
         port: 5432
       },
     production: {
-      user: "xmsqbexfqtkohz",
-      host: "ec2-54-217-219-235.eu-west-1.compute.amazonaws.com",
-      database: "d1c5h9fthlio6v",
-      password: "abfd04c30a9116c93273ea647906218800f975ba79c437bfce98ae890e0a0e96",
-      port: 5432,
-      ssl: true
+      connectionString: process.env.DATABASE_URL
     }
   };
