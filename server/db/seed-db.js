566de573e7fe1197003d3c3476ac7fa72dfb18c7
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-08-15 23:46:21 +0100

    fix eslint errors

--- server/db/seed-db.js
+++ server/db/seed-db.js
@@ -10,15 +10,13 @@ const populateDb = fs
 	.toString();
 const pool = new Pool(config);
 
-console.log("yo");
-
 queryDb(createSchema)
 	.then(() => queryDb(populateDb))
 	.then(() => process.exit(0));
 
 function queryDb(query) {
 	return new Promise((resolve, error) => {
-		pool.query(query, function(err, result) {
+		pool.query(query, function(err) {
 			if (err) {
 				console.log("error: ", err);
 				error(err);
