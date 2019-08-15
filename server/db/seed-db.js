06fcd97085af10ae0da29c5a8ceb3edd27c56da7
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-08-15 23:45:47 +0100

    test formatting

--- server/db/seed-db.js
+++ server/db/seed-db.js
@@ -2,27 +2,29 @@ const { Pool } = require("pg");
 const config = require("../config");
 const fs = require("fs");
 
-const createSchema = fs.readFileSync(require.resolve("./recreate-schema.sql")).toString();
-const populateDb = fs.readFileSync(require.resolve("./populate-db.sql")).toString();
+const createSchema = fs
+	.readFileSync(require.resolve("./recreate-schema.sql"))
+	.toString();
+const populateDb = fs
+	.readFileSync(require.resolve("./populate-db.sql"))
+	.toString();
 const pool = new Pool(config);
 
-console.log('yo');
+console.log("yo");
 
 queryDb(createSchema)
-  .then(() => queryDb(populateDb))
-  .then(() => process.exit(0));
+	.then(() => queryDb(populateDb))
+	.then(() => process.exit(0));
 
 function queryDb(query) {
-  return new Promise((resolve, error) => {
-    pool.query(query, function(err, result) {
-      if (err) {
-        console.log("error: ", err);
-        error(err);
-        process.exit(1);
-      }
-      resolve();
-    });
-  });
+	return new Promise((resolve, error) => {
+		pool.query(query, function(err, result) {
+			if (err) {
+				console.log("error: ", err);
+				error(err);
+				process.exit(1);
+			}
+			resolve();
+		});
+	});
 }
-
-
